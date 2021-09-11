c0f32304e5a9f44807e9335ff5db76b5925143c2
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-11 22:14:10 +0530

    improved style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css"
 import { AboutSection } from "./Components/AboutSection"
 import { ContactSection } from "./Components/ContactSection"
 import { HeroSection } from "./Components/HeroSection"
+import { Navbar } from "./Components/Navbar"
 import { ProjectsSection } from "./Components/ProjectsSection"
 import { SkillsSection } from "./Components/SkillsSection"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HeroSection />
       <AboutSection />
       <SkillsSection />
